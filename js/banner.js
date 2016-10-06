efc3c67e2d8e999c59a5bb5545ca1bc3c4e02e51
Brandon Furtwangler <brandon.furtwangler@hotmail.com>
2016-10-06 13:59:32 -0700

    Site updated: 2016-10-06 13:59:32

--- js/banner.js
+++ js/banner.js
@@ -1,4 +1,4 @@
-var numBanners = 1;
+var numBanners = 2;
 var bannerIndex = Math.floor(Math.random() * numBanners);
 var iframe = document.getElementById('banner-iframe');
 iframe.src = 'https://brandf.github.io/brandf-banner-' + bannerIndex;
